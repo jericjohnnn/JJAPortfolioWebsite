e9e12d8aad5275c8c459da9e71d30b0a6539038a
JJ👨‍💻 <112809736+jericjohnnn@users.noreply.github.com>
2025-04-20 14:50:44 +0800

    update

--- src/components/MyWorksWeb.vue
+++ src/components/MyWorksWeb.vue
@@ -78,20 +78,6 @@ const carouselItems = [
     ],
   },
   {
-    image: '/works_dev/wealthme.webp',
-    title: 'WealthME',
-    description: 'An expense tracker that logs income and expenses by category to help you make smarter financial decisions with AI.',
-    technologies: [
-      'https://skillicons.dev/icons?i=php',
-      'https://skillicons.dev/icons?i=js',
-      'https://skillicons.dev/icons?i=laravel',
-      'https://skillicons.dev/icons?i=vue',
-      'https://skillicons.dev/icons?i=mysql',
-      'https://skillicons.dev/icons?i=vuetify',
-    ],
-    links: [{ label: 'Live demo', icon: 'globe', url: 'https://wealthme.site/' },],
-  },
-  {
     image: '/works_dev/passafun.webp',
     title: 'Passaquiz',
     description:
@@ -105,8 +91,22 @@ const carouselItems = [
       'https://skillicons.dev/icons?i=tailwind',
     ],
     links: [
-      { label: 'Live demo', icon: 'globe', url: 'https://dev.play.passafund.com/' },
+      { label: 'Live demo', icon: 'globe', url: 'https://play.passafund.com/' },
     ],
+  },
+  {
+    image: '/works_dev/wealthme.webp',
+    title: 'WealthME',
+    description: 'An expense tracker that logs income and expenses by category to help you make smarter financial decisions with AI.',
+    technologies: [
+      'https://skillicons.dev/icons?i=php',
+      'https://skillicons.dev/icons?i=js',
+      'https://skillicons.dev/icons?i=laravel',
+      'https://skillicons.dev/icons?i=vue',
+      'https://skillicons.dev/icons?i=mysql',
+      'https://skillicons.dev/icons?i=vuetify',
+    ],
+    links: [{ label: 'Live demo', icon: 'globe', url: 'https://wealthme.site/' },],
   },
   // {
   //   image: '/works_dev/jja.webp',
